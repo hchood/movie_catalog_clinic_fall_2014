850985cb443e91cface9604ee73d16baa553fcf9
Helen Hood <helen.c.hood@gmail.com>
2014-08-27 15:24:57 -0400

    Add get_all_actors method for actors index page

--- server.rb
+++ server.rb
@@ -2,3 +2,44 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 require 'pg'
+
+# USER VIEWS A LIST OF ACTORS:
+# Visiting /actors will show a list of actors, sorted alphabetically by name.
+# Each actor name is a link to the details page for that actor.
+
+#####################################
+              # METHODS
+#####################################
+
+def db_connection
+  begin
+    connection = PG.connect(dbname: 'movies')
+
+    yield(connection)
+  ensure
+    connection.close
+  end
+end
+
+def get_all_actors
+  query = %Q{
+    SELECT * FROM actors
+    ORDER BY name;
+  }
+
+  results = db_connection do |conn|
+    conn.exec(query)
+  end
+
+  results.to_a
+end
+
+#####################################
+              # ROUTES
+#####################################
+
+get '/actors' do
+  @actors = get_all_actors
+
+  erb :'actors/index'
+end
