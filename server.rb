80a02fa5a75ceadbb79691d50c8273ba6b0c98f7
Helen Hood <helen.c.hood@gmail.com>
2014-08-28 11:13:18 -0400

    Extract SQL query into a VIEW

--- server.rb
+++ server.rb
@@ -83,14 +83,8 @@ end
 
 def get_movie_info(movie_id)
   query = %Q{
-    SELECT movies.title, movies.year, movies.id, movies.rating, genres.name AS genre, studios.name AS studio,
-    actors.id AS actor_id, actors.name AS actor, cast_members.character AS role
-    FROM movies
-    JOIN genres ON genres.id = movies.genre_id
-    JOIN studios ON studios.id = movies.studio_id
-    JOIN cast_members ON cast_members.movie_id = movies.id
-    JOIN actors ON actors.id = cast_members.actor_id
-    WHERE movies.id = $1;
+    SELECT * FROM movie_cast_members
+    WHERE id = $1;
   }
 
   results = db_connection do |conn|
