70ed2070fc0d6c292866b42624470a4840085bee
Helen Hood <helen.c.hood@gmail.com>
2014-08-29 15:40:18 -0400

    Include synopsis in movie info view

--- movie_information.sql
+++ movie_information.sql
@@ -1,5 +1,5 @@
 CREATE VIEW movie_information AS
-  SELECT movies.title, movies.year, movies.id, movies.rating, genres.name AS genre, studios.name AS studio
+  SELECT movies.title, movies.year, movies.id, movies.rating, movies.synopsis, genres.name AS genre, studios.name AS studio
   FROM movies
   JOIN genres ON genres.id = movies.genre_id
   JOIN studios ON studios.id = movies.studio_id;
